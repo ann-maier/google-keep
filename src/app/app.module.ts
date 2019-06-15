2a277878add34390c7cacd956394793cfc5f3792
Anna Maier <anna_maier@epam.com>
2019-06-15 16:09:14 +0300

    add ngrx store,effects and change folder structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/app/app.component';
 
 @NgModule({
   declarations: [
